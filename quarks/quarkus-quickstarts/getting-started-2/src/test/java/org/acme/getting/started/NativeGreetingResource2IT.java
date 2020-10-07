a5b26c96131c9910a8140e780e1436250f3607fb
Mykola Bova <mykola.bova@shopit.com>
2020-10-07 15:31:22 +0300

    some updates

---
+++ quarks/quarkus-quickstarts/getting-started-2/src/test/java/org/acme/getting/started/NativeGreetingResource2IT.java
@@ -0,0 +1,9 @@
+package org.acme.getting.started;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResource2IT extends GreetingResource2Test {
+
+    // Execute the same tests but in native mode.
+}
